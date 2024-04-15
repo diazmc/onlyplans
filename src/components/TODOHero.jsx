a41c86943bb534f00bf096904f433de0f8722635
diazmc <mc.arthur_d@hotmail.com>
2024-04-14 19:53:34 -0700

    change text

--- src/components/TODOHero.jsx
+++ src/components/TODOHero.jsx
@@ -2,7 +2,7 @@ function TODOHero({ todos_completed, total_todos }) {
   return (
     <section className="todohero_section">
       <div>
-        <p className="text_large">Plan Done</p>
+        <p className="text_large">Plans Done</p>
         <p className="text_small">Keep it up</p>
       </div>
       <div>
